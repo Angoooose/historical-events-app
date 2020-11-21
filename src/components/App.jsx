11f7e16c9cab47921f6d68162ac981a22f37ad96
Angoose <68403129+Angoooose@users.noreply.github.com>
2020-11-21 12:31:19 -0600

    Adding Header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
 import '../styles/App.css';
 
+import Header from './Header.jsx';
+
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [headerTheme, setHeaderTheme] = useState('#2196f3');
+  const [textTheme, setTextTheme] = useState('black');
+
+  useEffect(() => {
+    if (isDarkMode) {
+      setHeaderTheme('#424242');
+      setTextTheme('white');
+      document.body.style.backgroundColor = '#303030';
+    } else {
+      setHeaderTheme('#2196f3');
+      setTextTheme('black');
+      document.body.style.backgroundColor = 'white';
+    }
+  }, [isDarkMode]);
+
   return (
-    null
+    <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} headerTheme={headerTheme} />
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+import '../styles/Header.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Brightness4Icon from '@material-ui/icons/Brightness4';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import gitHubLogo from '../assets/GitHub-Mark-Light-120px-plus.png';
+
+export default function Header(props) {
+    const { isDarkMode, setIsDarkMode, headerTheme } = props;
+    const [ displayDarkButton, setDisplayDarkButton ] = useState('block');
+    const [ displayLightButton, setDisplayLightButton ] = useState('none');
+
+    function setTheme() {
+        if (!isDarkMode) {
+            setIsDarkMode(true);
+            setDisplayLightButton('block');
+            setDisplayDarkButton('none');
+        } else {
+            setIsDarkMode(false);
+            setDisplayDarkButton('block');
+            setDisplayLightButton('none');
+        }
+    }
+
+    const headerStyles = makeStyles((theme) => ({
+        root: {
+            flexGrow: 1,
+        },
+        appBar: {
+            backgroundColor: headerTheme,
+        },
+        title: {
+            flexGrow: 1,
+            fontSize: 20,
+        },
+        brightnessIconDark: {
+            display: displayDarkButton,
+            height: 40,
+            marginRight: 10,
+            transform: 'scale(1)',
+            transition: '250ms',
+            width: 40,
+            '&:hover': {
+                cursor: 'pointer',
+                transform: 'scale(1.1)',
+                transition: '250ms',
+            },
+        },
+        brightnessIconLight: {
+            display: displayLightButton,
+            height: 40,
+            marginRight: 10,
+            transform: 'scale(1)',
+            transition: '250ms',
+            width: 40,
+            '&:hover': {
+                cursor: 'pointer',
+                transform: 'scale(1.1)',
+                transition: '250ms',
+            },
+        },
+    }));
+
+    const classes = headerStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar className={classes.appBar}>
+                <Toolbar>
+                    <Typography className={classes.title}> 
+                        Historical Events
+                    </Typography>
+                    <Brightness4Icon className={classes.brightnessIconDark} onClick={() => setTheme()} />
+                    <Brightness7Icon className={classes.brightnessIconLight} onClick={() => setTheme()} />
+                    <a href="https://github.com/Angoooose/historical-events-app">
+                        <img src={gitHubLogo} className="github-img"></img>
+                    </a>
+                </Toolbar>
+            </AppBar>
+        </div>
+      );
+}
