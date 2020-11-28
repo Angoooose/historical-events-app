83d1533226a296bc20108b1be73b5fc327793143
Angoose <68403129+Angoooose@users.noreply.github.com>
2020-11-27 20:12:01 -0600

    Implementing mobile support

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,8 @@ import Cards from './Cards.jsx';
 import GatherEvents from '../api/GatherEvents';
 import Footer from './Footer.jsx';
 
+import { useMediaQuery } from 'react-responsive'
+
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [headerTheme, setHeaderTheme] = useState('#2196f3');
@@ -16,6 +18,8 @@ function App() {
   const [isNeedWeeklyEvents, setIsNeedWeeklyEvents] = useState(true);
   const [weeklyEvents, setWeeklyEvents] = useState(['', '', '', '', '', '', '']);
   const [weeklyEventsYears, setWeeklyEventsYears] = useState(['', '', '', '', '', '', '']);
+  const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+  const isVerySmallScreen = useMediaQuery({ query: '(max-device-width: 320px)' });
 
   useEffect(() => {
     if (isDarkMode) {
@@ -34,7 +38,7 @@ function App() {
       <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} headerTheme={headerTheme} />
       <Cards headerTheme={headerTheme} bodyTextTheme={bodyTextTheme} dailyEvents={dailyEvents} dailyEventsYears={dailyEventsYears} weeklyEvents={weeklyEvents} weeklyEventsYears={weeklyEventsYears} />
       <GatherEvents isNeedDailyEvents={isNeedDailyEvents} setIsNeedDailyEvents={setIsNeedDailyEvents} dailyEvents={dailyEvents} setDailyEvents={setDailyEvents} dailyEventsYears={dailyEventsYears} setDailyEventsYears={setDailyEventsYears} isNeedWeeklyEvents={isNeedWeeklyEvents} setIsNeedWeeklyEvents={setIsNeedWeeklyEvents} weeklyEvents={weeklyEvents} setWeeklyEvents={setWeeklyEvents} weeklyEventsYears={weeklyEventsYears} setWeeklyEventsYears={setWeeklyEventsYears} />
-      <Footer headerTheme={headerTheme} />
+      <Footer headerTheme={headerTheme} isPortrait={isPortrait} isVerySmallScreen={isVerySmallScreen} />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,19 @@
 import '../styles/Footer.css';
 
 export default function Footer(props) {
-    const { headerTheme } = props;
+    const { headerTheme, isPortrait, isVerySmallScreen } = props;
+
+    let fontSize = 'normal-font';
+    
+    if (isPortrait && !isVerySmallScreen) {
+        fontSize = 'small-font';
+    } else if (isVerySmallScreen) {
+        fontSize = 'very-small-font';
+    }
 
     return (
         <div className="footer" style={ {backgroundColor: headerTheme} }>
-            <div className="footer-text">
+            <div className={`footer-text ${fontSize}`}>
                 All events are gathered using the <a href="https://history.muffinlabs.com/">Today in History</a> API.
             </div>
         </div>
