818a22959a13338c7356b236ab38c8c246383ca2
Angoose <68403129+Angoooose@users.noreply.github.com>
2020-11-27 20:10:06 -0600

    Updating Cards component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,7 +8,7 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 
 export default function Cards(props) {
-    const { headerTheme, bodyTextTheme, dailyEvents, dailyEventsYears, weeklyEvents, weeklyEventsYears} = props;
+    const { headerTheme, bodyTextTheme, dailyEvents, dailyEventsYears, weeklyEvents, weeklyEventsYears } = props;
 
     const daysArray = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '14th', '15th', '16th', '17th', '18th', '19th', '20th', '21st', '22nd', '23rd', '24th', '25th', '26th', '27th', '28th', '29th', '30th', '31st'];
     const monthsArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -62,8 +62,8 @@ export default function Cards(props) {
             marginBottom: '8px',
             marginLeft: '8px',
             marginRight: '8px',
-            maxWidth: '15%',
-            minWidth: '15%',
+            maxWidth: '40%',
+            minWidth: '40%',
             textAlign: 'center',
         },
     }));
